6a05cc8cf15b70e440af75214b25b295b776cbd0
pivotal-preston-sheldon <psheldon@pivotal.io>
2018-05-20 07:26:56 -0400

    Add commandline param to substitute image name

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -4,7 +4,10 @@
 # Author: psheldon@pivotal.io
 # Description: Build script for building extended Oracle Docker image with pre-built database.
 #
-docker build --force-rm=true --no-cache=true -t ps-oracle . || {
+
+export IMAGE_NAME=${1:-ps-oracle:12.2.0.1}
+echo "Creating Image: " ${IMAGE_NAME}
+docker build --force-rm=true --no-cache=true -t $IMAGE_NAME . || {
   echo ""
   echo "ERROR: Oracle Database Docker Image was NOT successfully created."
   echo "ERROR: Check the output and correct any reported problems with the docker build operation."
